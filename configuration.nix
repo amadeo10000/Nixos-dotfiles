412060f642c1240669a9276d9bfa1f3ef5c5f150
amadeo10000 <135474129+amadeo10000@users.noreply.github.com>
2025-10-24 11:44:26 -0300

    Add files via upload
    conf.nix and hardwre.nix, soon i may implement flakes or sumthin

---
+++ configuration.nix
@@ -0,0 +1,157 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # enable bluetooth
+  hardware.bluetooth.enable = true;
+
+
+  # Set your time zone.
+  time.timeZone = "America/Argentina/Buenos_Aires";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "es_AR.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "es_AR.UTF-8";
+    LC_IDENTIFICATION = "es_AR.UTF-8";
+    LC_MEASUREMENT = "es_AR.UTF-8";
+    LC_MONETARY = "es_AR.UTF-8";
+    LC_NAME = "es_AR.UTF-8";
+    LC_NUMERIC = "es_AR.UTF-8";
+    LC_PAPER = "es_AR.UTF-8";
+    LC_TELEPHONE = "es_AR.UTF-8";
+    LC_TIME = "es_AR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "latam";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "la-latin1";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sharko = {
+    isNormalUser = true;
+    description = "sharko";
+    shell = pkgs.fish;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+
+
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+   neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   wget
+   kdePackages.sddm
+   hyprland
+   fish
+   kitty
+   fastfetch
+   wayland
+   brave
+   wofi
+   waybar
+   steam
+   swww
+   brightnessctl
+   nerd-fonts.departure-mono
+   noto-fonts-cjk-sans
+   noto-fonts-cjk-serif
+   pipewire
+   mpv
+   mpd
+   pwvucontrol
+   obsidian
+   bluetui
+   bluez
+
+];
+
+
+	programs.hyprland.enable = true;
+	programs.fish.enable = true;
+	
+
+	    fonts.packages = with pkgs; [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      nerd-fonts.departure-mono
+    ];
+
+	security.rtkit.enable = true;
+
+  # Desactivar pulseaudio (PipeWire lo reemplaza)
+  services.pulseaudio.enable = false;
+
+  # Habilitar servicios PipeWire
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;  # sonido para apps de 32 bits
+    pulse.enable = true;       # provee compatibilidad con PulseAudio
+    jack.enable = true;        # opcional, para apps que usan JACK
+    wireplumber.enable = true;
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
